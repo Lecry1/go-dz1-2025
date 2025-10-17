c1be15f77a5720f5d80735f01276af0af235ad82
Vlad Glebov <glebov.vs.vs@gmail.com>
2025-10-17 15:01:36 +0300

    fix problems

--- internal/task_1/filterCommonDigits.go
+++ internal/task_1/filterCommonDigits.go
@@ -1,60 +1,51 @@
 package task1
 
+func GetDigit(num int) [10]bool {
+	var digits[10] bool
+	for tmp:= num; tmp > 0; tmp /= 10 {
+		d := tmp % 10
+		digits[d] = true
+	}
+	return digits
+}
+
+func NewDigit(num int, digits *[10] bool) int {
+	newNum := 0
+	multNew := 1
+	for tmpNum := num; tmpNum > 0; tmpNum /= 10 {
+		digit := tmpNum % 10
+		if !(*digits)[digit] {
+			newNum = multNew * digit + newNum
+			multNew *= 10
+		}
+	}
+	return newNum
+}
+
+
 func FilterCommonDigits(a, b int) (int, int, error){
 	if a < 0 || b < 0 {
 		return 0, 0, ErrNegNums
 	} 
 	
 	// --=-=-=-=-=-=-=-=-=-
-	var digitA[10] bool
-	var digitB[10] bool
 
-	tmpA := a
-	for tmpA > 0 {
-		d := tmpA % 10
-		digitA[d] = true
-		tmpA /= 10
-	}
+	digitA := GetDigit(a)
+	digitB := GetDigit(b)
 
-	tmpB := b
-	for tmpB > 0 {
-		d := tmpB % 10
-		digitB[d] = true
-		tmpB /= 10
-	}
 	// -=-=-=-=--=-=-=-=-=-
 
 	var common[10] bool
-	for i := 0; i < 10; i++ {
+	for i := range 10 {
 		if digitA[i] && digitB[i] {
 			common[i] = true
 		}
 	}
 	// -=-=-=-=-=-=-=-=-=-
 
-	newA := 0
-	tmpA = a
-	multA := 1
-	for  tmpA > 0 {
-		d := tmpA % 10
-		if !common[d] {
-			newA = multA * d + newA
-			multA *= 10
-		}
-		tmpA /= 10
-	}
+	newA := NewDigit(a, &common)
+	newB := NewDigit(b, &common)
 
-	newB := 0
-	tmpB = b
-	multB := 1
-	for tmpB > 0{
-		d := tmpB % 10
-		if !common[d] {
-			newB += multB * d
-			multB *= 10
-		}
-		tmpB /= 10
-	}
 	if ( newA == 0 && newA != a) || ( newB == 0 && newB != b){
 		return 0,0, ErrEmptyNum
 	}
--- internal/task_2/findCommonWords.go
+++ internal/task_2/findCommonWords.go
@@ -3,7 +3,7 @@ package task2
 import (
 	"fmt"
 	"os"
-	"strings"
+	"bufio"
 )
 
 func FindCommonWords(outputFilename string, inputFilenames ...string) error {
@@ -15,25 +15,38 @@ func FindCommonWords(outputFilename string, inputFilenames ...string) error {
 	// m = make(map[string]int)
 	m := make(map[string]int)
 	for _, filename := range inputFilenames {
-		data, err := os.ReadFile(filename) // вообще это плохое решение, весь файл в память плохо пихать
+		file, err := os.Open(filename)
 		if err != nil {
 			return ErrOpenFile
 		}
-		text := string(data)
-		for _ , word := range strings.Fields(text) {
-			m[word] += 1
+		defer file.Close()
+		
+		scanner := bufio.NewScanner(file)
+		scanner.Split(bufio.ScanWords)
+		
+		for scanner.Scan() {
+			word := scanner.Text()
+			m[word]++
+		}
+		
+		if err := scanner.Err(); err != nil {
+			return err
 		}
 	}
+
 	fileOut, err := os.OpenFile(outputFilename, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
 	if err != nil{
 		return ErrOpenFile
 	}
+	defer fileOut.Close()
+
+
 	for key, value := range m {
 		if len(inputFilenames) == value  {
 			fmt.Fprint(fileOut ,key)
 		}
 	}
-	defer fileOut.Close()
+	
 
 	return nil
 }
